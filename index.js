dc94ac39033bb1afdccbba591028e5bce70fab83
RoberioAlves <roberiouol@gmail.com>
2024-04-15 16:13:34 -0300

    autalizando

--- index.js
+++ index.js
@@ -40,21 +40,12 @@ app.get('/users', (request, response) => {
 })
 
 app.post('/users', (request, response) => {
-    try{
-    const { name, age} = request.body
+    const { name, age } = request.body;
+    const user = { id: uuid.v4(), name, age };
+    users.push(user);
+    return response.status(201).json(user);
+});
 
-    if(age < 18) throw new error('user is under 18 years old')
-
-    const user = {id: uuid.v4(), name, age}
-
-    users.push(user)
-    return response.status(201).json({user})
-} catch(err) {
-    return response.status(400).json({error: err.message})
-} finally {
-    console.log('Compra finalizada.')
-}
-})
 
 app.put('/users/:id', checkUSerId, (request, response) => {
     
